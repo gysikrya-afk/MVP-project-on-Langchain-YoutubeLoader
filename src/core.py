9fe8c821d0446d1c9e039f435dcea662653bf35f
Neo <gysi.krya@gmail.com>
2026-01-05 11:36:05 +0200

    Create llm in core.py
    Добавлена функция create_llm для создания LLM с использованием ChatGroq и PromptTemplate.

--- src/core.py
+++ src/core.py
@@ -1,1 +1,30 @@
+from langchain_groq import ChatGroq
+from langchain_classic.prompts import PromptTemplate
 
+def create_llm(groq_api_key):
+    """
+    Создание LLM
+    
+    :param groq_api_key: API-KEY Groq
+
+    Returns:
+        chain:Готовая до роботы LLM
+    """
+
+    llm = ChatGroq(
+        model='',
+        temperature=0,
+        groq_api_key=groq_api_key
+    )
+
+    prompt = PromptTemplate(
+        input_variables=['loader_video'],
+        template="""Ты ИИ-Агент по анализу видео из Youtube.
+        Твоя задача давать краткий отчёт о видео
+        
+        Видео:{loader_video}"""
+    )
+
+    chain = prompt | llm
+
+    return chain
